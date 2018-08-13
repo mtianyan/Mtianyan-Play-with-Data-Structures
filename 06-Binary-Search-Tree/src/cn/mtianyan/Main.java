8cfe7a8b3831f2c5abc8d12eb5e1ab19cc9266bd
mtianyan <1147727180@qq.com>
2018-08-13 14:23:52 +0800

    完成了二分搜索树底层实现相关代码

---
+++ 06-Binary-Search-Tree/src/cn/mtianyan/Main.java
@@ -0,0 +1,45 @@
+package cn.mtianyan;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        BST<Integer> bst = new BST<>();
+        int[] nums = {5, 3, 6, 8, 4, 2};
+        for(int num: nums)
+            bst.add(num);
+        /////////////////
+        //      5      //
+        //    /   \    //
+        //   3    6    //
+        //  / \    \   //
+        // 2  4     8  //
+        /////////////////
+        bst.preOrder();
+        System.out.println();
+
+        bst.inOrder(); // 2 3 4 5 6 8
+        System.out.println();
+
+        bst.postOrder(); // 2 4 3 8 6 5
+        System.out.println();
+
+        bst.preOrderNR(); // 5 3 2 4 6 8
+        System.out.println();
+
+        System.out.printf("非递归中序: ");
+        bst.inOrderNR();
+        System.out.println();
+
+        System.out.printf("非递归后序: ");
+        bst.postOrderNR();
+        System.out.println();
+
+        System.out.printf("队列实现层序遍历: ");
+        bst.levelOrder();
+        System.out.println();
+
+        System.out.println(bst);
+    }
+}
+
